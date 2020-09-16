156507242e1adcf250357761384a1daba603bc0c
Vasilkov Ilya <viv.vasilkov@gmail.com>
2020-09-16 16:04:30 +0700

    Add Factories && Fix mysql container

--- src/database/factories/TariffFactory.php
+++ src/database/factories/TariffFactory.php
@@ -2,18 +2,18 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Models\Tariff;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class TariffFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
      *
      * @var string
      */
-    protected $model = User::class;
+    protected $model = Tariff::class;
 
     /**
      * Define the model's default state.
@@ -24,10 +24,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'price' => $this->faker->numberBetween(1, 100000),
+            'day_mask' => $this->faker->regexify('[0-1]{7}'),
         ];
     }
 }
